a535e1b69fa8f26de47cc477e237a0ed7c7642ea
HaaVad <123933986+HaaVad@users.noreply.github.com>
2025-08-23 15:15:32 +0200

    Alma

--- Assets/Scripts/Combat/PlayerController.cs
+++ Assets/Scripts/Combat/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour{
     public float moveSpeed = 6f;
+    public float jumpForce = 10f; // Added jump force
 
     private Rigidbody2D rb;
     private Controls controls;
     private Vector2 move;
+    private bool isGrounded = true; // Simple grounded check
 
     void Awake()
     {
@@ -17,6 +19,8 @@ public class PlayerController : MonoBehaviour{
         // Read Move action
         controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
         controls.Player.Move.canceled  += ctx => move = Vector2.zero;
+        // Read Jump action
+        controls.Player.Jump.performed += ctx => Jump();
     }
 
     void OnEnable()  => controls.Enable();
@@ -25,5 +29,23 @@ public class PlayerController : MonoBehaviour{
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Simple ground check using collision
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts.Length > 0 && collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
     }
 }
